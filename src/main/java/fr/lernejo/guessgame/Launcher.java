86686b221ea5a6577a0e22954587f6a9d74e53d8
Carineee <chen.carine@hotmail.fr>
2022-10-10 02:45:12 +0200

    Partie4

--- src/main/java/fr/lernejo/guessgame/Launcher.java
+++ src/main/java/fr/lernejo/guessgame/Launcher.java
@@ -5,9 +5,23 @@ import java.security.SecureRandom;
 public class Launcher {
 
     public static void main(String[] args) {
-        Simulation simulation = new Simulation(new HumanPlayer());
-        SecureRandom random = new SecureRandom();
-// long randomNumber = random.nextLong(); // génère un nombre entre Long.MIN_VALUE et Long.MAX_VALUE
-        long random Number = random.nextInt(100);// génère un nombre entre 0 (inclus) et 100 (exclus)
+        if (args[0].equals("-interactive")) {
+            Simulation simulation = new Simulation(new HumanPlayer());
+            SecureRandom random = new SecureRandom();
+            //long random Number = random.nextInt(100);
+            simulation.initialize(random.nextInt(101));
+            simulation.loopUntilPlayerSucceed(Long.MAX_VALUE);
+        }
+
+        else if(args[0].equals("-auto")){
+            Simulation sm = new Simulation(new ComputerPlayer());
+            sm.initialize(Long.parseLong(args[1]));
+            sm.loopUntilPlayerSucceed(1000);
+        }
+
+        else {
+            
+        }
+
     }
 }
