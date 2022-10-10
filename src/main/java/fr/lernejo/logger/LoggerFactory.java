c15213b582ec0b84ad02e1f92fed0b8a8882aee5
Carineee <chen.carine@hotmail.fr>
2022-10-10 18:07:21 +0200

    Partie6

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -4,9 +4,9 @@ import java.io.File;
 
 public class LoggerFactory {
     public static Logger getLogger(String name){
-        return new ContextualLogger(name, new ConsoleLogger());
+        //return new ContextualLogger(name, new ConsoleLogger());
         //return new ContextualLogger(name, new FileLogger("log/log.txt"));
-        //return new CompositeLogger(new ContextualLogger(name, new ConsoleLogger()), new ContextualLogger(name, new FileLogger("log/log.txt")));
+        return new CompositeLogger(new ContextualLogger(name, new ConsoleLogger()), new ContextualLogger(name, new FileLogger("log/log.txt")));
         //return new CompositeLogger(new ContextualLogger(name, new ConsoleLogger()), new FilteredLogger(new ContextualLogger(name, new FileLogger("log/log.txt")), message -> message.contains("simulation")));
         //return new CompositeLogger(new ContextualLogger(name, new ConsoleLogger()), new ContextualLogger(name, new FilteredLogger(new FileLogger("history.log"), message -> message.contains("simulation"))));
     }
